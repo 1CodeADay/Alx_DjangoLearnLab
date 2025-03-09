2ffbb209e953810eb70b6e5ed4e1915aafa45ad5
Rodrigue Danso <rodrig.danso@gmail.com>
2025-03-09 14:21:36 +0000

    update 1

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -14,7 +14,7 @@ urlpatterns = [
     path("library/<int:pk>/", LibraryDetailView.as_view(), name="library_detail"),
     path("", home, name="home"),  # Ajoute la page d'accueil ici
 
-    path('login/', auth_views.LoginView.as_view(), name='login'),  # Use built-in LoginView
-    path('logout/', auth_views.LogoutView.as_view(), name='logout'),  # Use built-in LogoutView
+    path('login/', auth_views.LoginView.as_view(template_name='registration/login.html'), name='login'),  # Use built-in LoginView
+    path('logout/', auth_views.LogoutView.as_view(template_name='registration/logout.html'), name='logout'),  # Use built-in LogoutView
     path('register/', views.register, name='register'),  # Custom register view
 ]
